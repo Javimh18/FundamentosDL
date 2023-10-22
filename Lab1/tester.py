135cb031d361e2aca9f12f4a812b680dd02da29e
Javier Muñoz <javier.munnozharo@estudiante.uam.es>
2023-10-22 12:07:25 +0200

    Pytorch model does not work very well at the moment...

--- Lab1/tester.py
+++ Lab1/tester.py
@@ -3,9 +3,11 @@ import tensorflow as tf
 from dlfbt_lab1 import NeuralNetwork
 import pickle
 import os 
+import dlfbt
 
 if __name__ == '__main__':
-    os.chdir('Practicas/FundamentosDL/Lab1')
+    os.chdir('FundamentosDeepLearning/Practicas/FundamentosDL/Lab1')
+
     np.random.seed(17)
     x = np.random.randn(3, 20)
     t = np.random.randn(1, 20)
@@ -19,10 +21,37 @@ if __name__ == '__main__':
     assert db[1].shape == (1, 1)
 
     # Array values, should match those on the test file:
-    with open('DLFBT-LAB/test_nn_numpy_compute_gradients.pickle', 'rb') as handle:
+    with open('./test_nn_numpy_compute_gradients.pickle', 'rb') as handle:
         [dWexp, dbexp] = pickle.load(handle)
 
     tol = 1.e-8
     for dwp, dbp, dwe, dbe in zip(dW, db, dWexp, dbexp):
         assert np.max(np.abs(dwp - dwe)) < tol
         assert np.max(np.abs(dbp - dbe)) < tol
+
+    dg = dlfbt.DataGeneratorLinear(a=[2.0, 2.0])
+    dg.create_dataset(n=1000, seed=17, noise=1.0)
+    x = dg.x.transpose()
+    t = dg.t.transpose()
+
+    np.random.seed(23)
+    net = NeuralNetwork([(2, 'input'), (1, 'linear')])
+
+    dW, db = net.compute_gradients(x, t)
+    
+    
+    loss = net.fit(x, t, 0.01, 100, 1000, NeuralNetwork.mse_loss)
+    z, y = net.predict(x)
+
+    assert z[0].shape == (1, 1000)
+    assert y[0].shape == (1, 1000)
+
+    # Array values, should match those on the test file:
+    with open('./test_nn_numpy_fit.pickle', 'rb') as handle:
+        [zexp, yexp] = pickle.load(handle)
+
+    tol = 1.e-8
+    for zp, yp, ze, ye in zip(z, y, zexp, yexp):
+        assert np.max(np.abs(zp - ze)) < tol
+        assert np.max(np.abs(yp - ye)) < tol
+    
--- Lab3/dlfbt_lab3.py
+++ Lab3/dlfbt_lab3.py
@@ -2,13 +2,16 @@
 # DLFBT 2023/2024
 # Lab assignment 3
 # Authors:
-#   Name1 NIA1
-#   Name2 NIA2
+#   Manuel Otero NIA1
+#   Javier Muñoz Haro NIA2
 #===============================================================================
 
 import numpy as np
 import tensorflow as tf
 import torch
+from torch import nn
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
 
 #===============================================================================
 # Exercise 1. Gradient descent to find the minimum of a function
@@ -147,4 +150,169 @@ class LinearRegressionModel_pytorch(object):
 #        should be well documented.
 #-----------------------------------------------------------------------
 
+def init_weights(m):
+    if isinstance(m, nn.Linear):
+        torch.nn.init.xavier_uniform_(m.weight)
+        m.bias.data.fill_(0.01)
 
+class CNN_model(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 16, 3)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(16, 32, 4)
+        self.conv3 = nn.Conv2d(32, 64, 3)
+        self.fc1 = nn.Linear(64 * 2 * 2, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+         # Using Xavier initilization
+        # self.comp_block.apply(init_weights)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.softmax(self.fc3(x), dim=1)
+        return x
+    
+# Calculate accuracy (a classification metric)
+def accuracy_fn(y_true, y_pred):
+    return (y_pred.round() == y_true).float().mean()
+
+def train(dataloader,
+          model,
+          loss_fn, 
+          optimizer,
+          device, 
+          n_epochs = 100,
+          print_interval = 2):
+    
+    """Tests a PyTorch model for a single epoch.
+
+    Turns a target PyTorch model to "eval" mode and then performs
+    a forward pass on a testing dataset.
+
+    Args:
+    model: A PyTorch model to be tested.
+    dataloader: A DataLoader instance for the model to be tested on.
+    loss_fn: A PyTorch loss function to calculate loss on the test data.
+    optimizer: function that optimizes the learning of the model
+    device: A target device to compute on (e.g. "cuda" or "cpu").
+    n_epochs: number of epochs for the model to be trained
+    print_inverval: 
+
+    Returns:
+    A dictionary with history arrays for training loss and training accuracy metrics.
+    """
+    
+    history_loss = [] 
+    history_acc = []
+    history = {}
+
+    for epoch in range(n_epochs):
+        train_loss = 0.0
+        train_acc = 0.0
+        for _, (X,y) in enumerate(dataloader, 0):
+            
+            X,y = X.to(device), y.to(device)
+            y_pred = model(X)
+
+            # get metrics for loss
+            loss = loss_fn(y_pred, y.long())
+            train_loss += loss
+
+            # get metrics for accuracy
+            y_pred_class = torch.argmax(y_pred, dim=1)
+            acc = accuracy_fn(y_true=y, y_pred=y_pred_class)
+            train_acc += acc
+
+            # reseting the optimizer
+            optimizer.zero_grad()
+            # backward pass (backpropagation)
+            loss.backward()
+            # adjusting the necessary weigths of our models
+            optimizer.step()
+
+        epoch_loss = train_loss/len(dataloader)
+        epoch_acc = train_acc/len(dataloader)
+
+        if (epoch + 1) % print_interval == 0:
+            print(f"Epoch: {epoch + 1} | Loss: {epoch_loss} | Accuracy: {epoch_acc}")
+
+        history_acc.append(epoch_acc.item())
+        history_loss.append(epoch_loss.item())
+
+        history['accuracy'] = history_acc
+        history['loss'] = history_loss
+
+    return history
+
+def test_step(model: torch.nn.Module, 
+              dataloader: torch.utils.data.DataLoader, 
+              loss_fn: torch.nn.Module,
+              device: torch.device):
+  """Tests a PyTorch model for a single epoch.
+
+  Turns a target PyTorch model to "eval" mode and then performs
+  a forward pass on a testing dataset.
+
+  Args:
+    model: A PyTorch model to be tested.
+    dataloader: A DataLoader instance for the model to be tested on.
+    loss_fn: A PyTorch loss function to calculate loss on the test data.
+    device: A target device to compute on (e.g. "cuda" or "cpu").
+
+  Returns:
+    A tuple of testing loss and testing accuracy metrics.
+    In the form (test_loss, test_accuracy). For example:
+
+    (0.0223, 0.8985)
+  """
+  # Put model in eval mode
+  model.eval() 
+
+  # Setup test loss and test accuracy values
+  test_loss, test_acc = 0, 0
+
+  # Turn on inference context manager
+  with torch.inference_mode():
+      # Loop through DataLoader batches
+      for batch, (X, y) in enumerate(dataloader):
+          # Send data to target device
+          X, y = X.to(device), y.to(device)
+
+          # 1. Forward pass
+          test_pred_logits = model(X)
+
+          # 2. Calculate and accumulate loss
+          loss = loss_fn(test_pred_logits, y)
+          test_loss += loss.item()
+
+          # Calculate and accumulate accuracy
+          test_pred_labels = test_pred_logits.argmax(dim=1)
+          test_acc += ((test_pred_labels == y).sum().item()/len(test_pred_labels))
+
+  # Adjust metrics to get average loss and accuracy per batch 
+  test_loss = test_loss / len(dataloader)
+  test_acc = test_acc / len(dataloader)
+  return test_loss, test_acc
+
+def plot_metrics(history:dict, n_epochs:int):
+    # plotting training statistics
+
+    plt.figure(figsize=(10,6))
+    plt.plot(history['accuracy'])
+    plt.title(f'Accuracy for our basic model. Training for {n_epochs}')
+    plt.ylabel('Accuracy')
+    plt.xlabel('Epoch')
+    plt.show()
+
+    plt.figure(figsize=(10,6))
+    plt.plot(history['loss'])
+    plt.title(f'Loss for our basic model. Training for {n_epochs}')
+    plt.ylabel('Loss')
+    plt.xlabel('Epoch')
+    plt.show()
